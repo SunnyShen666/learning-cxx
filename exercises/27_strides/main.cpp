8bfeee8ca0f4cbfdb0a3ac5d0c5ce64642cad961
SunnyShen666 <2449383203a@gmail.com>
2026-07-16 16:22:08 +0800

    complete all exercises

--- exercises/27_strides/main.cpp
+++ exercises/27_strides/main.cpp
@@ -18,6 +18,14 @@ std::vector<udim> strides(std::vector<udim> const &shape) {
     // TODO: 完成函数体，根据张量形状计算张量连续存储时的步长。
     // READ: 逆向迭代器 std::vector::rbegin <https://zh.cppreference.com/w/cpp/container/vector/rbegin>
     //       使用逆向迭代器可能可以简化代码
+    udim current_stride = 1;
+
+    for(std::size_t i=shape.size();i>0;--i)
+    {
+        strides[i-1]=current_stride;
+        current_stride *= shape[i-1];
+    }
+
     return strides;
 }
 
--- exercises/33_std_accumulate/main.cpp
+++ exercises/33_std_accumulate/main.cpp
@@ -12,6 +12,16 @@ int main(int argc, char **argv) {
     //       - 连续存储；
     //       的张量占用的字节数
     // int size =
+    int elements = std::accumulate(
+        std::begin(shape),
+        std::end(shape),
+        1,
+        [](int result,int dimension)
+        {
+            return result*dimension;
+        }
+    );
+    int size = elements * sizeof(DataType);
     ASSERT(size == 602112, "4x1x3x224x224 = 602112");
     return 0;
 }
